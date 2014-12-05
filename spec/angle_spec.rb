9f5aa69ea5c5b94bc7c68943cc0c32f05e2cdb53
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-12-05 07:11:22 -0700

    added specs for rad -> deg conversion

--- spec/angle_spec.rb
+++ spec/angle_spec.rb
@@ -8,4 +8,10 @@ describe "Angle" do
 		expect(SolCal::Angle.from_rad(1.0).to_rad).to be_within(0.001).of(1.0)
 		expect(SolCal::Angle.from_rad(2.2).to_rad).to be_within(0.001).of(2.2)
 	end
+
+	it "should return deg value of angle" do
+		expect(SolCal::Angle.from_rad(10*180/Math::PI).to_deg).to be_within(0.1).of(10)
+		expect(SolCal::Angle.from_rad(45*180/Math::PI).to_deg).to be_within(0.1).of(45)
+		expect(SolCal::Angle.from_rad(93*180/Math::PI).to_deg).to be_within(0.1).of(93)
+	end
 end
