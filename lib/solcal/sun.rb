cffd6f3e47d3869f5fe088212144bbe40ee0f963
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-12-19 14:17:13 -0700

    removed unused methods from sun

--- lib/solcal/sun.rb
+++ lib/solcal/sun.rb
@@ -1,13 +1,5 @@
 module SolCal
 	module Sun
-		def self.true_anomoly(geometric_mean_anom, equation_of_center)
-			geometric_mean_anom.to_deg+equation_of_center
-		end
-
-		def self.rad_vector(eccent_earth_orbit, true_anomoly)
-			1.000001018*(1-eccent_earth_orbit*eccent_earth_orbit)/(1+eccent_earth_orbit*Math.cos(true_anomoly.to_rad))
-		end
-
 		def self.daylight(latitude, longitude, time_zone, date)
 			results = {date:date,time_zone:time_zone,latitude:latitude,longitude:longitude}
 			SolCal::Commands::SunsetCommand.new(results).execute
--- spec/sun_spec.rb
+++ spec/sun_spec.rb
@@ -1,21 +1,12 @@
 require 'spec_helper'
 
 describe "Sun" do
-
 	before :each do
 		@julian_century = 0.149125142596397
 		@latitude = SolCal::Angle.from_deg(53.5333)
 		@longitude = SolCal::Angle.from_deg(-113.5)
 		@time_zone = -7
 		@date = Date.new(2014,12,1)
-	end
-
-	it "should calculate the sun's true anomoly" do
-		expect(SolCal::Sun.true_anomoly(SolCal::Angle.from_deg(5725.893), -1.092)).to be_within(0.001).of(BigDecimal.new(5724.801,9))
-	end
-
-	it "should calculate the sun's radian vector" do
-		expect(SolCal::Sun.rad_vector(BigDecimal.new(0.016702,9), SolCal::Angle.from_deg(5725.893))).to be_within(0.001).of(0.986261)
 	end
 
 	it "should calculate the interesting daylight information" do
